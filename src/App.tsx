5291ffc0b7b647b55040573a1a1e082e43c10438
o1ghol <suzu@nunu.org>
2026-05-25 00:52:23 +0900

    App.tsxから不要な機能を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
 import DeckGL from "@deck.gl/react";
 import { Map as MapGL } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
-import { useState } from "react";
-import { DrowmodeManager } from "./components/drow-mode-manager";
+import { DrowModeToolbar } from "./components/drow-mode-manager";
 import { FeatureManager } from "./components/feature-manager";
-import PointBox from "./components/feature-manager/point/PointBox";
 
 export default function App() {
-  //点のテスト用データ
-  const [testPoint, setTestPoint] = useState({
-    id: "dummy-point-1",
-    latitude: 35.6812,
-    longitude: 139.7671,
-    altitude: 10,
-    color: { r: 15, g: 118, b: 110, a: 255 },
-  });
   return (
     <div>
+      {/* featuremanager */}
       <div style={{ position: "absolute", zIndex: 50 }}>
         <FeatureManager />
       </div>
-      {/* 点のテスト用UI */}
-      <div
-        style={{
-          position: "absolute",
-          zIndex: 50,
-          top: "4rem",
-          left: "1rem",
-        }}
-      >
-        <PointBox
-          key={testPoint.id}
-          point={testPoint}
-          onUpdate={(_, updates) =>
-            setTestPoint((prev) => ({ ...prev, ...updates }))
-          }
-          onDelete={() => {}}
-        />
-      </div>
+
+      {/* drowmodemanager */}
       <div
         style={{
           position: "absolute",
@@ -46,8 +21,9 @@ export default function App() {
           right: "1rem",
         }}
       >
-        <DrowmodeManager />
+        <DrowModeToolbar />
       </div>
+      {/* map */}
       <DeckGL
         initialViewState={{
           longitude: 139.767,
