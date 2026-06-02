102652ce35a0765ad2b8d77a676f6608d06f826b
nAgI314 <kurosawasyunnagi@gmail.com>
2026-06-02 10:02:32 +0900

    共通パネルコンポーネントを追加

---
+++ src/components/feature-manager/common-ui/Panel.tsx
@@ -0,0 +1,25 @@
+import styles from "./Panel.module.scss";
+
+type CommonPanelProps = {
+  children: React.ReactNode;
+  footerButton?: React.ReactNode;
+};
+
+/**
+ * 各featureで使用するパネル
+ */
+export default function CommonPanel({ children, footerButton }: CommonPanelProps) {
+  return (
+    <div className={styles.panelContainer}>
+      <div className={styles.scrollArea}>
+        <div className={styles.itemList}>
+          {children}
+        </div>
+      </div>
+
+      <div className={styles.footer}>
+        {footerButton}
+      </div>
+    </div>
+  );
+}
