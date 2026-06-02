408be13dbd150bf38b96441557c20dd011314748
nAgI314 <kurosawasyunnagi@gmail.com>
2026-06-03 01:51:59 +0900

    linepanelを追加

---
+++ src/components/feature-manager/line/LineBox.tsx
@@ -0,0 +1,74 @@
+import { IconTarget, IconTrash } from "@tabler/icons-react";
+import type { Line } from "../../../types/geometry/line";
+import IconButton from "../common-ui/IconButton";
+import ColorButton from "../common-ui/ColorButton";
+import PositionInput from "../common-ui/PositionInput";
+import FeatureItemBox from "../common-ui/FeatureItemBox";
+import styles from "./LineBox.module.scss";
+
+type LineBoxProps = {
+  line: Line;
+  onUpdate: (id: string, updates: Partial<Line>) => void;
+  onDelete: (id: string) => void;
+};
+
+/**
+ * 起点・終点の緯度・経度・高度の入力欄と、削除・フォーカス・カラーの操作ボタンを持つボックス一つのセット
+ */
+export default function LineBox({ line, onUpdate, onDelete }: LineBoxProps) {
+  const color = line.color ?? { r: 15, g: 118, b: 110, a: 255 };
+
+  return (
+    <FeatureItemBox
+      actions={
+        <>
+          <IconButton
+            onClick={() => onDelete(line.id)}
+            ariaLabel="線を削除"
+            variant="danger"
+          >
+            <IconTrash />
+          </IconButton>
+
+          <IconButton
+            onClick={() => {
+              // todo
+            }}
+            ariaLabel="線に移動"
+          >
+            <IconTarget />
+          </IconButton>
+
+          <ColorButton
+            color={color}
+            ariaLabel="色を変更"
+            onClick={() => {
+              // todo
+            }}
+          />
+        </>
+      }
+    >
+      <div className={styles.lineContent}>
+        <PositionInput
+          id={`${line.id}-start`}
+          latitude={line.start.latitude}
+          longitude={line.start.longitude}
+          altitude={line.start.altitude}
+          onChange={(updates) =>
+            onUpdate(line.id, { start: { ...line.start, ...updates } })
+          }
+        />
+        <PositionInput
+          id={`${line.id}-end`}
+          latitude={line.end.latitude}
+          longitude={line.end.longitude}
+          altitude={line.end.altitude}
+          onChange={(updates) =>
+            onUpdate(line.id, { end: { ...line.end, ...updates } })
+          }
+        />
+      </div>
+    </FeatureItemBox>
+  );
+}
